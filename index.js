7dd48c7c1c655ed9552fa118c53b76c9b752b5fc
WilberthGamboa <wgcampos15@gmail.com>
2023-08-29 23:50:43 -0600

    mapeando y generando el json

--- index.js
+++ index.js
@@ -1,32 +1,99 @@
 // Analizando 
 import { categoryModel } from './src/models/category.model.js';
-
-import * as ExcelJS from "exceljs";
-
+import exceljs from 'exceljs';
+import { excelCategory } from './src/models/excelCategory.model.js';
+const { Workbook } = exceljs;
+excelCategory
+let objetoConJSON = {
+  metadata: [
+      
+  ]
+};
 //const ExcelJS = require('exceljs');
 async function main() {
 
-    const workbook = new ExcelJS.default.Workbook();
+    const workbook = new Workbook();
     const excel = await workbook.xlsx.readFile('datosexcel.xlsx');
    const worksheet= excel.getWorksheet('TABLA')
    let count = 0;
-   const startingColumnIndex = 3;
+   const startingColumnIndex = 1;
   worksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
     if (rowNumber===4) {
+      row.eachCell((cell,collnumber)=>{
+
+       
+ 
+          const title = worksheet.getCell(1,collnumber).value;
+          for (const key in excelCategory) {
+            if (excelCategory.hasOwnProperty(key)) {
+              
+              const excelValue = excelCategory[key].excel;
+              const dspaceValue = excelCategory[key].dspace;
+              if (title===excelValue) {
+              
+                if (cell.value==='x') {
+                  const cambiox = worksheet.getCell(2,collnumber).value;
+                  objetoConJSON.metadata.push({key:'example',value:cambiox})
+                  
+                  
+                } else {
+                  if (cell.value.richText) {
+                 //   console.log(cell.value.richText)
+              
+                    const richText =cell.value.richText
+                    let text = '';
+                    richText.forEach(element => {
+                      text = text + element.text
+                    });
+                    objetoConJSON.metadata.push({key:'example',value:text.toString()})
+                  }else{
+                    objetoConJSON.metadata.push({key:'example',value:cell.value})
+                  }
+                 
+                  
+                }
+                
+     
+             // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
+            }
+          }
+        }
+      })
+      /*
+      const x = Array.from(row.values);
+      x.forEach((element,index) => {
+       
+      });
+      /*
+      for (const key in excelCategory) {
+        if (excelCategory.hasOwnProperty(key)) {
+          
+          const excelValue = excelCategory[key].excel;
+          const dspaceValue = excelCategory[key].dspace;
+         // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
+        }
+      }
+      /*
         console.log(rowNumber)
         const x = Array.from(row.values);
         const nueva = x.map((element,index) => {
-            if (element==='x') {
-              const celdaData= worksheet.getCell(1,index).value;
-              if (celdaData!=undefined) {
-                console.log(String(celdaData))
-              }
-              return celdaData;
+          if (element) {
+           
+            const celdaData= worksheet.getCell(1,index).value;
+            console.log(String(celdaData))
+          }
+            //if (element==='x') {
+              //const celdaData= worksheet.getCell(1,index).value;
+              //if (celdaData!=undefined) {
+               // console.log(String(celdaData))
+             // }
+             // return celdaData;
                 
-            }
+           // }
+        
         });
         
-    
+    */
    
     }
    
@@ -37,6 +104,7 @@ async function main() {
         console.table(`Fila ${rowNumber}, Columna ${columnIndex}: ${cellValue}`);
       }*/
   });
+  console.log(objetoConJSON)
 }
 +
 main()
--- src/models/excelCategory.model.js
+++ src/models/excelCategory.model.js
@@ -1,7 +1,7 @@
 const prefixArq = 'arq  ';
 const prefixDc = 'dc';
 
-const excelCategory = {
+export const excelCategory = {
     nombre: { excel: 'Nombre', dspace: `${prefixArq}Nombre` },
     direccion: {excel:'Dirección',dspace:`${prefixArq}Direccion`},
     pertence: {excel:'Pertenece a',dspace:`${prefixArq}Direccion`},
@@ -11,5 +11,23 @@ const excelCategory = {
     jurisdiccionEclesiasticaFundacion: {excel:'Jurisdiccion eclesiastica de su fundacion',dspace:`${prefixArq}Direccion`},
     categoriaActual:{excel:'Categoria actual',dspace:`${prefixArq}Direccion`},
     categoriaOriginal: {excel:'Categoría original',dspace:`${prefixArq}Direccion`},
-    epocaConstruccion: {excel:'Epoca de construcción',dspace:`${prefixArq}Direccion`}
+    epocaConstruccion: {excel:'Epoca de construcción',dspace:`${prefixArq}Direccion`},
+    anioConstruccion: {excel:'Año de construcción y fechas significativas',dspace:`${prefixArq}Direccion`},
+    usoActual: {excel:'usoActual',dspace:`${prefixArq}Direccion`},
+    fundador: {excel:'fundador',dspace:`${prefixArq}Direccion`},
+    constructor: {excel:'constructor',dspace:`${prefixArq}Direccion`},
+    estadoConservacion:{excel:'Estado de conservación',dspace:`${prefixArq}Direccion`},
+    tipologia:{excel:'Tipología',dspace:`${prefixArq}Direccion`},
+    tipologiaArquitectonicaIglesias:{excel:'Tipología arquitectónica de las iglesias',dspace:`${prefixArq}Direccion`},
+    muros:{excel:'muros',dspace:`${prefixArq}Direccion`},
+    sistemaCubiertaOriginal:{excel:'Sistema de cubierta original',dspace:`${prefixArq}Direccion`},
+    sistemaEstructuralPresbiterio:{excel:'Sistema estructural del presbiterio',dspace:`${prefixArq}Direccion`},
+    sistemaEstructuralCoro:{excel:'Sistema estructural del coro',dspace:`${prefixArq}Direccion`},
+    pisos:{excel:'pisos',dspace:`${prefixArq}Direccion`},
+    acabados:{excel:'acabados',dspace:`${prefixArq}Direccion`},
+    materiales:{excel:'materiales',dspace:`${prefixArq}Direccion`},
+    gallinasCiegas:{excel:'Gallinas ciegas',dspace:`${prefixArq}Direccion`},
+    bienesMueblesElementosSignificativosRelevantes:{excel:'Bienes muebles y elementos significativos relevantes',dspace:`${prefixArq}Direccion`},
+    observacionesBienesMueblesElementosSignificativosRelevantes:{excel:'Observaciones de bienes muebles y elementos significativos relevantes',dspace:`${prefixArq}Direccion`},
+    observacionesGenerales:{excel:'Observaciones generales',dspace:`${prefixArq}Direccion`},
 }
