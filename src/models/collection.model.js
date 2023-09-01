9e5d85fd8891e09de7ed0303a20a6117bc48ffb8
WilberthGamboa <wgcampos15@gmail.com>
2023-08-31 23:49:57 -0600

    agregando el mapeo de colleciones

---
+++ src/models/collection.model.js
@@ -0,0 +1,51 @@
+const collectionId = {
+    quintanaRoo: {
+        nombreExcel: 'Quintana Roo', categorias: [{
+            itemExcelName: 'Capilla',
+            idDspace: '4761ccb0-b001-4993-8c51-f24f3f9b2c30'
+        },
+        {
+            itemExcelName: 'Parroquia',
+            idDspace: '3afc9bc0-024c-4a1b-8fe8-e758473f4f82'
+        },
+        {
+            itemExcelName: 'Convento',
+            idDspace: '29b09ef5-f3f6-4db2-8742-2f4d6d0a7d85'
+        },
+
+        ]
+    },
+    campeche: {
+        nombreExcel: 'Campeche', categorias: [{
+            itemExcelName: 'Capilla',
+            idDspace: '24ac0efd-dffb-422b-992c-2166e55b53df'
+        },
+        {
+            itemExcelName: 'Parroquia',
+            idDspace: '905ea2ce-b820-494d-958e-a298932d73f9'
+        },
+        {
+            itemExcelName: 'Convento',
+            idDspace: '5b300f08-36a6-4872-b0b4-47f31a2b3660'
+        },
+
+        ]
+    },
+    yucatan: {
+        nombreExcel: 'Yucatán', categorias: [{
+            itemExcelName: 'Capilla',
+            idDspace: 'ac1a06ab-df77-4ed1-84ce-c99f3a4c5ec1'
+        },
+        {
+            itemExcelName: 'Parroquia',
+            idDspace: 'a04b8ffa-baf9-441b-856e-ba1a26e87017'
+        },
+        {
+            itemExcelName: 'Convento',
+            idDspace: 'e077e4f1-2f4c-4a6d-a0df-ff62a87375b2'
+        },
+
+        ]
+    }
+
+}
--- src/service/import.service.js
+++ src/service/import.service.js
@@ -6,124 +6,137 @@ import { LoginController } from '../controllers/login.controller.js';
 
 const { Workbook } = exceljs;
 export class ImportService {
-    constructor() {
-        this.filePath = 'datosexcel.xlsx';
-        this.tableName = 'TABLA';
-         
-        
+  constructor() {
+    this.filePath = 'datosexcel.xlsx';
+    this.tableName = 'TABLA';
 
-    }
 
-    excelToDspace = async () => {
+
+  }
+
+  excelToDspace = async () => {
+
+    // Se instancia el excel, se obtiene de que archivo y tabla se sacará la información
+    const workbook = new Workbook();
+    const excel = await workbook.xlsx.readFile('datosexcel.xlsx');
+    const worksheet = excel.getWorksheet('TABLA')
+    //Se hace un bucle que recorra todas las lineas de nuestro documento (por filas)
+    worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+
+      // el rowNumber es tal cual el número de fila en el excel
+      if (rowNumber >2) {
+        // Objeto el cual se crea para mandar al servidor
         let objetoConJSON = {
-            metadata: [
-                
-            ]
-          };
-        // Se instancia el excel, se obtiene de que archivo y tabla se sacará la información
-        const workbook = new Workbook();
-        const excel = await workbook.xlsx.readFile('datosexcel.xlsx');
-        const worksheet = excel.getWorksheet('TABLA')
-        worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
-            if (rowNumber === 4) {
-                row.eachCell((cell, collnumber) => {
-                    const title = worksheet.getCell(1, collnumber).value;
-                    for (const key in excelCategory) {
-                        if (excelCategory.hasOwnProperty(key)) {
+          metadata: [
 
-                            const excelValue = excelCategory[key].excel;
-                            const dspaceValue = excelCategory[key].dspace;
-                            if (title === excelValue) {
+          ]
+        };
+        let idCollection;
+        /*
+          El row nos devuelvo un arreglo las celdas, por lo que
+          utilizamos el siguiente bucle para recorrer dicho arreglo
+        */
+        row.eachCell((cell, collnumber) => {
 
-                                if (cell.value === 'x') {
-                                    const cambiox = worksheet.getCell(2, collnumber).value;
-                                    objetoConJSON.metadata.push({ key: dspaceValue, value: cambiox })
+          const titleWorksheet = worksheet.getCell(1, collnumber).value;
 
+          for (const key in excelCategory) {
 
-                                } else {
-                                    if (cell.value.richText) {
-                                        //   console.log(cell.value.richText)
+            if (excelCategory.hasOwnProperty(key)) {
+              const excelValue = excelCategory[key].excel;
+              const dspaceValue = excelCategory[key].dspace;
 
-                                        const richText = cell.value.richText
-                                        let text = '';
-                                        richText.forEach(element => {
-                                            text = text + element.text
-                                        });
-                                        objetoConJSON.metadata.push({ key: dspaceValue, value: text.toString() })
-                                    } else {
-                                        objetoConJSON.metadata.push({ key: dspaceValue, value: cell.value })
-                                    }
+              if (title === excelValue) {
+
+                if (cell.value === 'x') {
+                  const cambiox = worksheet.getCell(2, collnumber).value;
+                  objetoConJSON.metadata.push({ key: dspaceValue, value: cambiox })
+
+                } else {
+                  if (cell.value.richText) {
 
 
-                                }
-
-
-                                // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
-                            }
-                        }
-                    }
-                })
-                /*
-                const x = Array.from(row.values);
-                x.forEach((element,index) => {
-                 
-                });
-                /*
-                for (const key in excelCategory) {
-                  if (excelCategory.hasOwnProperty(key)) {
-                    
-                    const excelValue = excelCategory[key].excel;
-                    const dspaceValue = excelCategory[key].dspace;
-                   // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
+                    const richText = cell.value.richText
+                    let text = '';
+                    richText.forEach(element => {
+                      text = text + element.text
+                    });
+                    objetoConJSON.metadata.push({ key: dspaceValue, value: text.toString() })
+                  } else {
+                    objetoConJSON.metadata.push({ key: dspaceValue, value: cell.value })
                   }
+
+
                 }
-                /*
-                  console.log(rowNumber)
-                  const x = Array.from(row.values);
-                  const nueva = x.map((element,index) => {
-                    if (element) {
-                     
-                      const celdaData= worksheet.getCell(1,index).value;
-                      console.log(String(celdaData))
-                    }
-                      //if (element==='x') {
-                        //const celdaData= worksheet.getCell(1,index).value;
-                        //if (celdaData!=undefined) {
-                         // console.log(String(celdaData))
-                       // }
-                       // return celdaData;
-                          
-                     // }
-                  
-                  });
-                  
-              */
 
+
+                // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
+              }
             }
-
-            /*
-            for (let columnIndex = startingColumnIndex; columnIndex <= row.cellCount; columnIndex++) {
-                const cell = row.getCell(columnIndex);
-                const cellValue = cell.value;
-                console.table(`Fila ${rowNumber}, Columna ${columnIndex}: ${cellValue}`);
-              }*/
+          }
+        })
+        /*
+        const x = Array.from(row.values);
+        x.forEach((element,index) => {
+         
         });
-        return objetoConJSON;
-    }
-
-    uploadItems = async () =>{
-        const data = await this.excelToDspace();
-        const loginController = new LoginController();
-        const cookies = await loginController.loginController();
-        const x = cookies[0]
-        const headers = {
-            'Content-Type': 'application/json',
-            'Cookie': x
-          };
-          console.log( x)
-          const response = await axios.post('http://148.209.67.83:8080/rest/collections/4761ccb0-b001-4993-8c51-f24f3f9b2c30/items', data,{headers});
-          console.log(response)
+        /*
+        for (const key in excelCategory) {
+          if (excelCategory.hasOwnProperty(key)) {
+            
+            const excelValue = excelCategory[key].excel;
+            const dspaceValue = excelCategory[key].dspace;
+           // console.log(`Clave: ${key}, Valor Excel: ${excelValue}, Valor DSpace: ${dspaceValue}`);
+          }
+        }
+        /*
+          console.log(rowNumber)
+          const x = Array.from(row.values);
+          const nueva = x.map((element,index) => {
+            if (element) {
+             
+              const celdaData= worksheet.getCell(1,index).value;
+              console.log(String(celdaData))
+            }
+              //if (element==='x') {
+                //const celdaData= worksheet.getCell(1,index).value;
+                //if (celdaData!=undefined) {
+                 // console.log(String(celdaData))
+               // }
+               // return celdaData;
+                  
+             // }
           
-    }
+          });
+          
+      */
+          // mandar info al submit
+
+      }
+
+      /*
+      for (let columnIndex = startingColumnIndex; columnIndex <= row.cellCount; columnIndex++) {
+          const cell = row.getCell(columnIndex);
+          const cellValue = cell.value;
+          console.table(`Fila ${rowNumber}, Columna ${columnIndex}: ${cellValue}`);
+        }*/
+    });
+    return objetoConJSON;
+  }
+
+  uploadItems = async (metadata, idCollection,) => {
+    const data = await this.excelToDspace();
+    const loginController = new LoginController();
+    const cookies = await loginController.loginController();
+    const x = cookies[0]
+    const headers = {
+      'Content-Type': 'application/json',
+      'Cookie': x
+    };
+    console.log(x)
+    const response = await axios.post('http://148.209.67.83:8080/rest/collections/4761ccb0-b001-4993-8c51-f24f3f9b2c30/items', data, { headers });
+    console.log(response)
+
+  }
 
 }
