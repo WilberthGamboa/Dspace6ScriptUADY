013aa6943bcfad34594c3dc969dddfb97574bb5b
WilberthGamboa <wgcampos15@gmail.com>
2023-09-08 00:12:35 -0600

    init get data

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 import { excelCategory } from './src/models/excelCategory.model.js';
 import { LoginController } from './src/controllers/login.controller.js';
 import { ImportController } from './src/controllers/import.controller.js';
+import { ImportService } from './src/service/import.service.js';
+import { DeleteService } from './src/service/delete.service.js';
 
 async function main() {
  try {
     const loginController = new LoginController();
     const sesionCookie = await loginController.loginController();
-   
+        /*
         const importController = new ImportController();
         importController.importController(sesionCookie);
+        */
+       const deleteService = new DeleteService();
+       deleteService.deleteItems();
+       
  
     
  } catch (error) {
--- src/service/delete.service.js
+++ src/service/delete.service.js
@@ -1,12 +1,13 @@
-class DeleteService {
+import axios from 'axios';
+export class DeleteService {
     constructor(){
-        this.serverURL = process.env.URLSERVIDOR
+        this.serverURL = `${process.env.URLSERVIDOR}/rest/items/?limit=500`
 
     }
 
     async deleteItems(cookieses){
         try {
-            const response = await axios.get(url, {
+            const response = await axios.get(this.serverURL, {
               headers: {
                 Cookie: cookieses,
               },
@@ -17,10 +18,34 @@ class DeleteService {
               process.exit(1);
             }
         
-            return response.data;
+            console.log(response.data)
           } catch (error) {
             console.error(error.message);
             process.exit(1);
           }
+    }
+
+     async  deleteItem2(itemID, jsessionID) {
+      const url = `http://${config.dspace.ip}:${config.dspace.port}/rest/items/${itemID}`;
+      const cookies = `JSESSIONID=${jsessionID}`;
+    
+      try {
+        const response = await axios.delete(url, {
+          headers: {
+            Cookie: cookies,
+          },
+        });
+    
+        if (response.status !== 200) {
+          console.error(response.status);
+          process.exit(1);
+        }
+    
+        // Handle the successful response if needed
+        // You can access the response data using response.data
+      } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+      }
     }
 }
