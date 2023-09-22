d066bae80fc76aa6ad73bc484e063bec3c944b81
WilberthGamboa <wgcampos15@gmail.com>
2023-09-22 00:29:11 -0600

    se mejora el funcionamiento de la subida de informacion

--- index.js
+++ index.js
@@ -3,23 +3,35 @@ import { LoginController } from './src/controllers/login.controller.js';
 import { ImportController } from './src/controllers/import.controller.js';
 import { DeleteService } from './src/service/delete.service.js';
 import readline from 'readline'
+import colors from 'colors'; // Import colors package
 
 async function main() {
 
 
  try {
-  
+   const inicio = process.hrtime();
+
+// Tu código aquí
+// Incluye llamadas asíncronas si es necesario
+
+
+
+
     const loginController = new LoginController();
     const sesionCookie = await loginController.loginController();
         
         const importController = new ImportController();
-          await importController.importController(sesionCookie); 
+      await importController.importController(sesionCookie); 
         //
         const deleteService = new DeleteService();
       //
-   //deleteService.deleteItem(sesionCookie);
-       
- 
+   //await deleteService.deleteItem(sesionCookie);
+   const fin = process.hrtime(inicio);
+   const tiempoDeEjecucionEnNanosegundos = fin[0] * 1e9 + fin[1];
+   const tiempoDeEjecucionEnSegundos = tiempoDeEjecucionEnNanosegundos / 1e9;
+
+   console.log(colors.yellow(`El tiempo de ejecución fue de ${tiempoDeEjecucionEnSegundos} segundos.`));
+
  
  } catch (error) {
     
